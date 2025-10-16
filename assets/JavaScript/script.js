41313414672b6262e3c5add9d6bee8cc6ac2aedb
Malik Robinson <malik16.mr@gmail.com>
2025-10-16 13:39:49 -0400

    HTML moved to JavaScript file + creation and call of GetHTMLStructure function

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -1,7 +1,38 @@
 function GetHTMLStructure() {
+
+  const bodyTag = document.querySelector('body');
+
+  bodyTag.innerHTML = `
+  <header class="header">
+    <h1>Friends On Demand</h1>
+    <h2>H2 test this is an addition for test purposes</h2>
+
+    <button>Test button</button>
+    <a href="#">Test Anchor link</a>
+  </header>
+
+  <!-- Test section to display of card container -->
+  <section class="card-grid">
+    <div class="card">
+      <h3>H3 test</h3>
+      <p>Brief description about this project goes here.</p>
+    </div>
+    <div class="card">
+      <h3>H3 test</h3>
+      <p>Some info about this project to show layout and contrast.</p>
+    </div>
+    <div class="card">
+      <h3>H3 test</h3>
+      <p>More placeholder content for visual comparison.</p>
+    </div>
+  </section>
+  
+  `;
 }
 
 document.addEventListener("DOMContentLoaded", function() {
   console.log("DOM content loaded");
+
+  GetHTMLStructure();
   
 });
