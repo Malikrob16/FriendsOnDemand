e528b88608ef84e1789d338992b91dae6ac66b98
Malik Robinson <malik16.mr@gmail.com>
2025-10-16 22:33:58 -0400

    Chat box functionality added

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -1,5 +1,51 @@
-function GetHTMLStructure() {
+const siteUser = "Malik Robinson";
 
+function GetCurrentTime() {
+  const currentTime = new Date();
+  let hours = currentTime.getHours();
+  let minutes = currentTime.getMinutes();
+  let seconds = currentTime.getSeconds(); 
+
+  if (minutes < 10) {
+    minutes = '0' + minutes;
+  }
+  if (seconds < 10) {
+    seconds = '0' + seconds;
+  }
+
+  const correctTime = `${hours}:${minutes}:${seconds}`;
+
+  console.log(correctTime);
+
+  return correctTime;
+
+}
+
+// This function will handle the input and output of the chat box.
+// It will take the users input and create a message to display in the chat box.
+// Will be changed down the line to also implement random messages from the characters watching with the user.
+function ChatBoxAugmenting(siteUser) {
+  const chatDisplay = document.getElementById('chat-display');
+  const chatInput = document.getElementById('chat-input');
+
+  chatInput.addEventListener("keyup", () => {
+
+    if (event.key === "Enter") {
+      event.preventDefault();
+
+      const chatMessage = chatInput.value;
+
+      if (chatMessage !== "") {
+        chatDisplay.innerHTML += `
+          <p>${siteUser}: ${chatMessage} . ${GetCurrentTime()}</p>
+        `;
+        chatInput.value = ""; // Clear input after message is sent
+      }
+    }
+  });
+}
+
+function GetHTMLStructure() {
   const bodyTag = document.querySelector('body');
 
   bodyTag.innerHTML = `
@@ -11,7 +57,7 @@ function GetHTMLStructure() {
     <a href="#">Test Anchor link</a>
   </header>
 
-  <!-- Test section to display of card container -->
+<!-- Test section to display of card container -->
   <section class="card-grid">
     <div class="card">
       <h3>H3 test</h3>
@@ -26,6 +72,14 @@ function GetHTMLStructure() {
       <p>More placeholder content for visual comparison.</p>
     </div>
   </section>
+
+  <section class="chat-container">
+    <div class="chat-display" id="chat-display">
+      <h4>Test for chat box</h4>
+    </div>
+
+    <input class="chat-input" id="chat-input" type="text" placeholder="Chat Here">
+  </section>
   
   `;
 }
@@ -34,5 +88,7 @@ document.addEventListener("DOMContentLoaded", function() {
   console.log("DOM content loaded");
 
   GetHTMLStructure();
+  ChatBoxAugmenting();
+  GetCurrentTime();
   
 });
